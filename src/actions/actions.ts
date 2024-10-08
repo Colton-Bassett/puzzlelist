a175692637bc34fca0540805d181e19b6bf704c3
Colton Bassett <coltonbassett@gmail.com>
2024-10-08 15:11:46 -0700

    Added User table to schema, PUT, and DELETE as well as revalidatePaths. Also added authWrapper

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
 
 export async function createPuzzle(formData: FormData) {
 	await prisma.puzzle.create({
@@ -11,4 +12,24 @@ export async function createPuzzle(formData: FormData) {
 			url: formData.get("url") as string,
 		},
 	});
+
+	// refresh ui to display new puzzles
+	revalidatePath("/");
+}
+
+// not implemented in UI
+export async function editPost(formData: FormData, id: string) {
+	await prisma.puzzle.update({
+		where: { id },
+		data: {
+			iconUrl: formData.get("iconUrl") as string,
+			name: formData.get("name") as string,
+			url: formData.get("url") as string,
+		},
+	});
+}
+
+// not implemented in UI
+export async function deletePuzzle(id: string) {
+	await prisma.puzzle.delete({ where: { id } });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import MostFollowedPuzzles from "./_components/home/mostFollowedPuzzles";
 import UserPuzzles from "./_components/home/userPuzzles";
 import UserEngagementPrompt from "./_components/home/userEngagementPrompt";
 import CreatePuzzleList from "./_components/home/createPuzzleList";
+import AuthWrapper from "./_components/authWrapper";
 import { createPuzzle } from "@/actions/actions";
 
 export default async function Home() {
@@ -12,50 +13,52 @@ export default async function Home() {
 	return (
 		<div className="">
 			<main className="flex h-full w-full flex-col">
-				<div className="min-h-full min-w-full">
-					<div className="h-20" style={{ backgroundColor: "#F8F9FA" }}></div>
-					<div
-						className="h-40 bg-bottom bg-no-repeat"
-						style={{ backgroundImage: "url(/landingpage.svg)" }}
-					></div>
-				</div>
-				<div className="flex items-center justify-center">
-					<form
-						action={createPuzzle}
-						className="flex w-[300px] flex-col gap-y-2"
-					>
-						<input
-							type="text"
-							name="name"
-							placeholder="Name"
-							className="rounded-sm px-2 py-1"
-						/>
-						<textarea
-							name="url"
-							rows={5}
-							placeholder="Url"
-							className="rounded-sm px-2 py-1"
-						/>
-						<button
-							type="submit"
-							className="rounded-sm bg-blue-500 py-2 text-white"
+				<AuthWrapper>
+					<div className="min-h-full min-w-full">
+						<div className="h-20" style={{ backgroundColor: "#F8F9FA" }}></div>
+						<div
+							className="h-40 bg-bottom bg-no-repeat"
+							style={{ backgroundImage: "url(/landingpage.svg)" }}
+						></div>
+					</div>
+					<div className="flex items-center justify-center">
+						<form
+							action={createPuzzle}
+							className="flex w-[300px] flex-col gap-y-2"
 						>
-							Create puzzle
-						</button>
-					</form>
-				</div>
-				<div className="m-auto flex h-full w-full max-w-5xl flex-col pb-6">
-					<UserEngagementPrompt />
+							<input
+								type="text"
+								name="name"
+								placeholder="Name"
+								className="rounded-sm px-2 py-1"
+							/>
+							<textarea
+								name="url"
+								rows={5}
+								placeholder="Url"
+								className="rounded-sm px-2 py-1"
+							/>
+							<button
+								type="submit"
+								className="rounded-sm bg-blue-500 py-2 text-white"
+							>
+								Create puzzle
+							</button>
+						</form>
+					</div>
+					<div className="m-auto flex h-full w-full max-w-5xl flex-col pb-6">
+						<UserEngagementPrompt />
 
-					<div className="m-auto mt-6 flex w-full max-w-5xl">
-						<UserPuzzles puzzles={puzzles} />
-						<div className="w-4/12">
-							<CreatePuzzleList />
-							<MostFollowedPuzzles puzzles={puzzles} />
+						<div className="m-auto mt-6 flex w-full max-w-5xl">
+							<UserPuzzles puzzles={puzzles} />
+							<div className="w-4/12">
+								<CreatePuzzleList />
+								<MostFollowedPuzzles puzzles={puzzles} />
+							</div>
 						</div>
 					</div>
-				</div>
-				<RecommendedPuzzles puzzles={puzzles} />
+					<RecommendedPuzzles puzzles={puzzles} />
+				</AuthWrapper>
 			</main>
 		</div>
 	);
