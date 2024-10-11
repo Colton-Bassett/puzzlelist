ad499c94957b412c7b3da6d2b1068f8f05df18b6
Colton Bassett <coltonbassett@gmail.com>
2024-10-11 15:00:50 -0700

    Improved MostFollowedPuzzles for mobile view, including ellipsis

--- src/app/_components/home/mostFollowedPuzzles.tsx
+++ src/app/_components/home/mostFollowedPuzzles.tsx
@@ -12,25 +12,27 @@ const MostFollowedPuzzles: React.FC<PuzzleProps> = async ({ puzzles }) => {
 	return (
 		<div className="mb-4 ml-6 rounded-xl border border-gray-200 p-4">
 			<div className="flex flex-col">
-				<div className="h-12 text-lg">Most followed on Puzzle List</div>
+				<div className="text-lg">
+					<span className="mb-3 block">Most followed on Puzzle List</span>
+				</div>
 				<ul className="list-disc">
 					{puzzles.slice(0, 4).map((puzzle) => (
 						<li
 							key={puzzle.id + "m"}
 							className="flex h-24 items-center border-t border-gray-200"
 						>
-							<div className="flex flex-1 flex-col overflow-hidden">
+							<div className="flex max-w-[130px] flex-col">
 								<div className="mr-3">img</div>
 								<div className="flex flex-1">{puzzle.name}</div>
 								<a
 									href={puzzle.url}
-									className="flex text-blue-500 hover:underline"
+									className="overflow-hidden text-ellipsis whitespace-nowrap text-blue-500 hover:underline"
 								>
 									{puzzle.url}
 								</a>
 							</div>
 
-							<div className="flex w-12 items-center justify-center">
+							<div className="flex w-10 flex-1 items-center justify-end">
 								<AddPuzzleButton
 									puzzleId={puzzle.id}
 									userPuzzles={userPuzzles}
